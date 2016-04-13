2c51cacbd7a10118e24fa7fa48443197cb81ebd4
rasmusj-se <rasmus@rtek.se>
2016-04-13 15:39:52 +0200

    Added upstart and modified installation script

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ See config.example and routes.example on how to configure RTMAIL.
 `go get -u && go build && chmod +x rtmail`
 
 `./rtmail -config ./config.example`
+
+## Installation
+
+Simply run the following to install a basic setup of RTMAIL.
+
+`curl https://raw.githubusercontent.com/rasmusj-se/rtmail/master/install.sh | bash`
+
+Change your config in /etc/rtmail/ and restart RTMAIL with `service rtmail restart`
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 curl https://build.rtek.se/rtmail > /usr/bin/rtmail
 chmod a+x /usr/bin/rtmail
-curl https://build.rtek.se/rtmail.conf > /etc/init/rtmail.conf
+curl https://raw.githubusercontent.com/rasmusj-se/rtmail/master/upstart.conf > /etc/init/rtmail.conf
 mkdir /etc/rtmail/
-curl https://build.rtek.se/rtmail.server.conf > /etc/rtmail/rtmail.conf
-curl https://build.rtek.se/rtmail.routes.conf > /etc/rtmail/routes.conf
+curl https://raw.githubusercontent.com/rasmusj-se/rtmail/master/config.example > /etc/rtmail/rtmail.conf
+curl https://raw.githubusercontent.com/rasmusj-se/rtmail/master/routes.example > /etc/rtmail/routes.conf
 openssl req -x509 -newkey rsa:4096 -keyout /etc/rtmail/cert.key -out /etc/rtmail/cert.crt -days 365
 service rtmail start
