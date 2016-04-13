2d86182af812cf7c225c4af31df9b4a8594b51f7
rasmusj-se <rasmus@rtek.se>
2016-04-13 17:40:44 +0200

    Added logging

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
 curl -s https://build.rtek.se/rtmail > /usr/bin/rtmail
 curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/scripts/rtmail-service.sh > /usr/bin/rtmail-service.sh
 chmod a+x /usr/bin/rtmail
 chmod a+x /usr/bin/rtmail-service.sh
-curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/scripts/upstart.conf > /etc/init/rtmail.conf
+curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/scripts/upstart.conf > /etc/init.d/rtmail.conf
+chmod +x /etc/init.d/rtmail.conf
+update-rc.d rtmail defaults
 mkdir /etc/rtmail/
 curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/rtmail.conf > /etc/rtmail/rtmail.conf
 curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/routes.conf > /etc/rtmail/routes.conf
