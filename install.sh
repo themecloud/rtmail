5b8daf1cfea145613b2ff0280e3448aec8b87f40
rasmusj-se <rasmus@rtek.se>
2016-04-13 16:39:43 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 curl -s https://build.rtek.se/rtmail > /usr/bin/rtmail
 chmod a+x /usr/bin/rtmail
-curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/upstart.config > /etc/init/rtmail.conf
+curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/upstart.config > /etc/init.d/rtmail
+chmod +x /etc/init.d/rtmail
 mkdir /etc/rtmail/
 curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/config.example > /etc/rtmail/rtmail.conf
 curl -s https://raw.githubusercontent.com/rasmusj-se/rtmail/master/routes.example > /etc/rtmail/routes.conf
 openssl req -new -newkey rsa:2048 -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=mail.example.com" -days 365 -nodes -x509 -keyout /etc/rtmail/cert.key -out /etc/rtmail/cert.crt
+useradd -r -s /bin/false rtmail
+setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/rtmail
+chown -R rtmail:rtmail /etc/rtmail
 service rtmail start
